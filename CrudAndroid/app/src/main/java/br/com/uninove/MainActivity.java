6ffb9eb06beb4dcfc1503b2f4b726284a8430f91
marco <lelis685@gmail.com>
2019-10-21 20:56:48 -0300

    adcionando imagem na home e bt sair

--- CrudAndroid/app/src/main/java/br/com/uninove/MainActivity.java
+++ CrudAndroid/app/src/main/java/br/com/uninove/MainActivity.java
@@ -1,19 +1,33 @@
 package br.com.uninove;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View.OnClickListener;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button sair;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        sair = findViewById(R.id.btSair);
+        sair.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+    }
 
     public void irTelaCadastro(View view){
         Intent intent = new Intent(this, CadastroActivity.class);
@@ -24,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ListarActivity.class);
         startActivity(intent);
     }
+
 
 
 }
