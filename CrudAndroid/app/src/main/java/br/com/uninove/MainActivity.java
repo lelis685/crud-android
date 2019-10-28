63c27b8c840df1983bc35362bb8865aeb4c7d52e
marco <lelis685@gmail.com>
2019-10-28 20:19:29 -0300

    ajuste bt sair

--- CrudAndroid/app/src/main/java/br/com/uninove/MainActivity.java
+++ CrudAndroid/app/src/main/java/br/com/uninove/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+                System.exit(0);
             }
         });
     }
